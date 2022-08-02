c227db5322670685e2c2a75d1135215004f72d42
brettstevens38 <38706144+brettstevens38@users.noreply.github.com>
2022-08-02 15:58:59 -0500

    Init IMU_CAN files

---
+++ SensorSquid/Inc/IMU_CAN.h
@@ -0,0 +1,22 @@
+/*
+ * IMU_CAN.h
+ *
+ *  Created on: Jul 31, 2022
+ *      Author: Brett Stevens
+ */
+
+#ifndef IMU_CAN_H_
+#define IMU_CAN_H_
+
+
+
+#endif /* IMU_CAN_H_ */
+
+struct imuState {
+	uint8_t x_translation = 0;
+	uint8_t y_translation = 0;
+	uint8_t z_translation = 0;
+	uint8_t x_rotation = 0;
+	uint8_t y_rotation = 0;
+	uint8_t z_rotation = 0;
+};
---
+++ SensorSquid/Src/IMU_CAN.c
@@ -0,0 +1,20 @@
+/*
+ * IMU_CAN.c
+ *
+ *  Created on: Jul 31, 2022
+ *      Author: Brett Stevens
+ */
+
+//Can datasheet
+//https://openimu.readthedocs.io/en/latest/software/CAN/CAN_J1939_Application.html
+//https://navview.blob.core.windows.net/web-resources/7430-3321-01%20User%20Manual%20OpenIMU335.pdf?_t=1621434422173
+
+//Can address = 0x80?
+
+#include "IMU_CAN.h"
+
+imuState state;
+
+void imuSetState(){
+
+}
