499db4cd1c4c2083fe8ac19943d901fcc1deb81e
Blane <bcypurda286@gmail.com>
2022-08-04 12:06:45 -0500

    Starting implementing the timer code

--- SensorSquid/Src/stm32f4xx_it.c
+++ SensorSquid/Src/stm32f4xx_it.c
@@ -75,6 +75,7 @@ extern DMA_HandleTypeDef hdma_adc1;
 extern DMA_HandleTypeDef hdma_sdio_tx;
 extern DMA_HandleTypeDef hdma_sdio_rx;
 extern SD_HandleTypeDef hsd;
+extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim10;
 
 /* USER CODE BEGIN EV */
@@ -185,6 +186,7 @@ void TIM1_UP_TIM10_IRQHandler(void)
   /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
 
   /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
   HAL_TIM_IRQHandler(&htim10);
   /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
 
